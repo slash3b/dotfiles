368d066927ab6056e4dbc4dd52cc8ee7282858a6
Ilya Tribusean <slash3b@gmail.com>
2025-10-26 19:34:12 +0200

    finally a much better version of goget

--- sources/config.fish
+++ sources/config.fish
@@ -1,8 +1,10 @@
 # "inspired" by Derek Taylor (DistroTube)
 
+# format script with `fish_indent --write your_script.fish`
+
 ### EXPORT ###
-set fish_greeting                                 # Supresses fish's intro message
-set EDITOR "nvim"                 		  # $EDITOR use Emacs in terminal
+set fish_greeting # Supresses fish's intro message
+set EDITOR nvim # $EDITOR use Emacs in terminal
 
 ### SET MANPAGER
 ### Uncomment only one of these!
@@ -55,7 +57,7 @@ set -Ua fish_user_paths "$HOME/go-pkgs/bin"
 
 # direnv 
 # trigger direnv at prompt, and on every arrow-based directory change (default)
-set -g direnv_fish_mode eval_on_arrow 
+set -g direnv_fish_mode eval_on_arrow
 
 # direnv fish config 
 direnv hook fish | source
@@ -79,31 +81,69 @@ function godoc
     go doc -all $argv | cat -n
 end
 
+
+# goget function will attempt to download golang version
+# then in case of a success it will nuke an entire go installation 
+# and replace with downloaded one.
 function goget
-    set_color green; echo "requested $argv"; set_color normal
+    if test (count $argv) -lt 1
+        echo "go version is required"
+        return 1
+    end
 
-    set -l dlfile "/home/slash3b/Downloads/$argv.linux-amd64.tar.gz"
+    # copy first argument
+    set -l goversion $argv[1]
 
-    if test ! -e $dlfile
-        set_color green; echo "\n\ndownloading https://go.dev/dl/go$argv.linux-amd64.tar.gz\n\n"; set_color normal
+    if not string match -q "go*" $goversion
+        set goversion "go$goversion"
+    end
 
-        wget --output-document "/home/slash3b/Downloads/$argv.linux-amd64.tar.gz" "https://go.dev/dl/$argv.linux-amd64.tar.gz"
+    set_color green
+    # fish uses 1-based indexing, so position 3 skips the first 2 characters
+    echo "Requested golang version:" (string sub -s 3 $goversion)
+    # echo $goversion
+    set_color normal
+
+
+    set -l dlfile "/home/slash3b/Downloads/$goversion.linux-amd64.tar.gz"
+    set -l dllink "https://go.dev/dl/$goversion.linux-amd64.tar.gz"
+
+    if test -e $dlfile
+        echo "$dlfile already downloaded"
+    else
+        wget --quiet --spider $dllink
+        if test $status -ge 1
+            echo "error, no such version exists: $dllink"
+
+            return 1
+        end
+
+        set_color green
+        printf "\ndownloading $dllink\n"
+        set_color normal
+
+        # download it to tmp
+        wget --output-document /tmp/source $dllink
 
         if test $status -ge 1
-            echo "failed to download https://go.dev/dl/$argv.linux-amd64.tar.gz"
+            echo "failed to download $dllink"
 
-            return
+            rm -f $dlfile
+
+            return 0
         end
-    else
 
-        echo "$argv tar.gz already downloaded"
+        # move to Downloads
+        mv /tmp/source $dlfile
     end
+
+    # safe to delete current go
 
     echo "deleting existing go...."
 
     rm -rf /home/slash3b/go
 
-    echo "unpacking $argv..."
+    echo "unpacking $goversion..."
 
     tar -C /home/slash3b/ -xzf $dlfile
     if test $status -ge 1
@@ -114,8 +154,7 @@ function goget
 
     set_color green
 
-    echo "installed $argv!"
+    echo "installed $goversion!"
 
 end
-
 
